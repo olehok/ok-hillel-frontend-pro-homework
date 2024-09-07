8b33bd7c5e41ab28c6cf8b61e85a57b19f4eb023
olehok <ok.olegkovalenko@gmail.com>
2024-09-07 22:14:35 +0300

    main clean

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log(typeof 1);
-console.log(typeof 'word');
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(`${typeof null} - null`);
-console.log(typeof 1n);
-console.log(typeof Symbol() );
-console.log(typeof {});
