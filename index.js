574d90dd137e774058e95514044a2e028f3fbb08
olehok <ok.olegkovalenko@gmail.com>
2024-09-07 21:26:24 +0300

    main branch

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(typeof 1);
+console.log(typeof 'word');
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(`${typeof null} - null`);
+console.log(typeof 1n);
+console.log(typeof Symbol() );
+console.log(typeof {});
