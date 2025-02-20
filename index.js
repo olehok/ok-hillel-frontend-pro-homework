526ca2f1af3364ca4261138f28b3f45ae3751322
olehok <ok.olegkovalenko@gmail.com>
2025-02-20 14:46:40 +0200

    6.1 includes method

---
+++ index.js
@@ -0,0 +1,19 @@
+// Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом.
+// 'func(" hello world", ['l', 'd'])' поверне нам "heo wor".
+//     Вихідний рядок та символи для видалення задає користувач.
+
+let initString = prompt('type the text');
+let initCutString = prompt('type the cutting text');
+let toCutArr = initCutString.split('');
+
+function cutFromString(str2Cut, arrCut) {
+    let initArr = str2Cut.split('');
+    let filteredArr = initArr.filter((element) => {
+        return !arrCut.includes(element)
+        }
+    )
+    return  filteredArr.join('')
+}
+
+let filteredString = cutFromString(initString, toCutArr);
+console.log(filteredString);
