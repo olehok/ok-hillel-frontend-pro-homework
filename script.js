5153eb8fea6ae42b5b91b16a36c0d33cbf26301d
olehok <ok.olegkovalenko@gmail.com>
2025-05-20 19:20:17 +0300

    task

---
+++ script.js
@@ -0,0 +1,9 @@
+// Створіть клас Calculator, який буде мати методи для виконання базових математичних операцій:
+// додавання, віднімання, множення та ділення.
+// class Calculator {
+// Ваш код тут}
+// const calc = new Calculator();
+// console.log(calc.add(5, 3)); // 8
+// console.log(calc.subtract(10, 4)); // 6
+// console.log(calc.multiply(3, 6)); // 18
+// console.log(calc.divide(8, 2)); // 4
