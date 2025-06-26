de98b3c0902aea1596f6b290772b32b42ebe732d
olehok <ok.olegkovalenko@gmail.com>
2025-06-26 16:32:43 +0300

    upd

--- script.js
+++ script.js
@@ -7,3 +7,28 @@
 // console.log(calc.subtract(10, 4)); // 6
 // console.log(calc.multiply(3, 6)); // 18
 // console.log(calc.divide(8, 2)); // 4
+
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b
+    }
+    multiply(a, b) {
+        return a * b
+    }
+    divide(a, b){
+        if (b === 0) {
+            return "Cannot divide by zero";
+        }
+        return a / b
+    }
+}
+
+const calc = new Calculator();
+console.log(calc.add(5, 3)); // 8
+console.log(calc.subtract(10, 4)); // 6
+console.log(calc.multiply(3, 6)); // 18
+console.log(calc.divide(8, 2)); // 4
+console.log(calc.divide(8, 0)); // Cannot divide by zero
