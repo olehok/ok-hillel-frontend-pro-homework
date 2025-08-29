24aac05ae0c23cedaf5626f7612df6ffbc9d31ad
olehok <ok.olegkovalenko@gmail.com>
2025-08-29 12:25:20 +0300

    start

---
+++ hw-react/src/components/Counter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.css'
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    render() {
+        return <div>
+            <h1>
+                hell o
+            </h1>
+        </div>
+    }
+}
+
+export default Counter;
