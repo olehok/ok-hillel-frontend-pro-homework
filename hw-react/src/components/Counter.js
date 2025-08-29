c990c3b2b960025888fce6cc5b3ca7063c02454f
olehok <ok.olegkovalenko@gmail.com>
2025-08-29 17:18:20 +0300

    counter

--- hw-react/src/components/Counter.js
+++ hw-react/src/components/Counter.js
@@ -5,15 +5,62 @@ class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            addition: 0
         };
     }
 
+    handleCount = (addition, isIncrement) => {
+        this.setState({
+            count: this.state.count + (isIncrement
+                ? addition
+                : -addition)
+        })
+    }
+
+    handleChangeAddition = (value) => {
+        this.setState({
+            addition: value
+        })
+    }
+
     render() {
-        return <div>
+        return <div className="container">
             <h1>
-                hell o
+                {this.state.count}
             </h1>
+            <p>
+                Актуальний доданок: {this.state.addition}
+            </p>
+            <div>
+                <button
+                    className="button"
+                    onClick={() => { this.handleChangeAddition(1) }}>
+                    1
+                </button>
+                <button
+                    className="button"
+                    onClick={() => { this.handleChangeAddition(5) }}>
+                    5
+                </button>
+                <button
+                    className="button"
+                    onClick={() => { this.handleChangeAddition(10) }}>
+                    10
+                </button>
+            </div>
+            <div>
+                <button
+                    className="button"
+                    onClick={() => { this.handleCount(this.state.addition, true) }}>
+                    додати {this.state.addition}
+                </button>
+                <button
+                    className="button"
+                    onClick={() => { this.handleCount(this.state.addition, false) }}>
+                    відняти {this.state.addition}
+                </button>
+            </div>
         </div>
     }
 }
